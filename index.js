588b0e8e5c1365cad4beeae7ddde9740f6fcb801
Devansh sharma <115212222+Devansh042005@users.noreply.github.com>
2025-05-20 23:26:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const express = require("express");
+const cookieParser = require('cookie-parser');
+const {restrictToLoggedInUserOnly, checkAuth} = require('./middleware/auth')
+const path = require('path');
 const app = express();
 const { connectToMongoDB } = require("./connect");
-const urlRoute = require("./routes/url");
 const URL = require("./models/url");
 
+const urlRoute = require("./routes/url");
+const staticRoute = require('./routes/staticRouter');
+const userRoute = require('./routes/user');
+
 const PORT = 8001;
+app.set("view engine", "ejs"); // setting up the ejs
+app.set('views' , path.resolve("./views")); // btaya sari ejs views me hai
+
 app.use(express.json());
+
+app.use(express.urlencoded({extended: false})) // these are middlewares
+app.use(cookieParser());
+
+app.use("/", checkAuth ,staticRoute);
+app.use("/url" , restrictToLoggedInUserOnly, urlRoute);
+app.use('/user', userRoute)
+
+
 
 connectToMongoDB("mongodb://localhost:27017/short-url").then(() =>
   console.log("Mongo DB connected")
-),
-  app.use("/url", urlRoute);
-app.get("/:shortId", async (req, res) => {
+);
+app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
     {
